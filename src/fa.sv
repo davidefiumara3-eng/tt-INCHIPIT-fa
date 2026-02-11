4d6fa89e3c5abe575707359de9858f6051351cf6
davidefiumara3-eng <davidefiumara3@gmail.com>
2026-02-11 12:14:19 +0100

    Update fa.sv

--- src/fa.sv
+++ src/fa.sv
@@ -11,12 +11,16 @@ module fa_module
         output logic Cout
     );
 
-    logic i0, i1, i2;
+    logic i0, i1, i2, i3;
 
     xor u0(i0, A, B);
 
     and u2(i1, A, B);
 
+    and u3(i2, i0, Cin);
+
     or u4(Cout, i1, i2);
+
+    xor u5(i3, i0, Cin);
 
 endmodule: fa_module
